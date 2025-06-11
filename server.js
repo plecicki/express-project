f2f4e19a186ca2997a73f22eaa11d5f907192402
Piotr Lecicki <p.lecicki@gmail.com>
2025-06-11 23:06:10 +0200

    first server

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/views/public')));
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>My first server!</h1>');
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>');
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.send('<h1>Info</h1>');
+  res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.send('<h1>History</h1>');
+  res.show('history.html');
 });
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
