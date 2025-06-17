cb3d7dd49a97a1e46249525e6fa1ef5f022bfdd7
Piotr Lecicki <p.lecicki@gmail.com>
2025-06-17 21:02:27 +0200

    Init front-end and open connection with server by cors

--- 26_5_chapter-project/server.js
+++ 26_5_chapter-project/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 
@@ -6,6 +7,7 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
